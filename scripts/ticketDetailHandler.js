3fd205be5b677f3b69ab8e92b15809794624656c
Sven van 't Hoenderdaal <sven@hoenderdaal.net>
2020-03-02 13:46:19 +0100

    Fallback for null ticket

--- scripts/ticketDetailHandler.js
+++ scripts/ticketDetailHandler.js
@@ -4,6 +4,9 @@ const urlParams = new URLSearchParams(queryString);
 setTicketInfo(urlParams.get('id'));
 
 function setTicketInfo(id) {
+    if (id == null) {
+        window.location = "https://asapcloud.netlify.com/tickets";
+    }
     document.title = id + " - ITSM";
     document.getElementById("ticketIdTitle").innerHTML = 'Ticket ' + id;
 }
